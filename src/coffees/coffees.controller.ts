4377a4bc6ace138f072486152beb69493eb3034d
Andrey Kotlyar <kotlyar562@gmail.com>
2025-04-27 22:12:05 +0300

    add create and update data dtos

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -3,6 +3,8 @@ import {
 } from '@nestjs/common';
 
 import { CoffeesService } from './coffees.service';
+import { CreateCoffeeDto } from './dto/create-coffee.dto';
+import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 
 @Controller('coffees')
 export class CoffeesController {
@@ -21,13 +23,13 @@ export class CoffeesController {
 
   @Post()
   @HttpCode(HttpStatus.GONE)
-  create(@Body() body) {
-    this.coffeesService.create(body);
+  create(@Body() createCoffeeDto: CreateCoffeeDto) {
+    this.coffeesService.create(createCoffeeDto);
   }
 
   @Patch(':id')
-  update(@Param('id') id: number, @Body() body) {
-    this.coffeesService.update(Number(id), body);
+  update(@Param('id') id: number, @Body() updateCoffeeDto: UpdateCoffeeDto) {
+    this.coffeesService.update(Number(id), updateCoffeeDto);
   }
 
   @Delete(':id')
