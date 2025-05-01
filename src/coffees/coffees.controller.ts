f345363b4eba6fdf7fbde2688cbe4febfc25c593
Andrey Kotlyar <kotlyar562@gmail.com>
2025-05-01 15:39:06 +0300

    add ValidationPipe not globally, but for a specific  module, a controller, a method or even a parameter

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,5 +1,18 @@
 import {
-    Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post, Put, Query, Res
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  Put,
+  Query,
+  Res,
+  UsePipes,
+  ValidationPipe,
 } from '@nestjs/common';
 
 import { CoffeesService } from './coffees.service';
@@ -7,10 +20,12 @@ import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { PaginationQueryDto } from './dto/pagination-query.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 
+// @UsePipes(ValidationPipe) // or @UsePipes(new ValidationPipe({configObject}))
 @Controller('coffees')
 export class CoffeesController {
   constructor(private readonly coffeesService: CoffeesService) {}
 
+  @UsePipes(ValidationPipe) // the same what on line 23, but only for this one method
   @Get()
   findAll(@Query() paginationQuery: PaginationQueryDto) {
     return this.coffeesService.findAll(paginationQuery);
@@ -38,7 +53,10 @@ export class CoffeesController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: number, @Body() updateCoffeeDto: UpdateCoffeeDto) {
+  update(
+    @Param('id') id: number,
+    @Body(ValidationPipe) updateCoffeeDto: UpdateCoffeeDto,
+  ) {
     this.coffeesService.update(id, updateCoffeeDto);
   }
 
