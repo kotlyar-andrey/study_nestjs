9f6b1d240c7f2c67dbc06a732fb7ae1c6a13b665
Andrey Kotlyar <kotlyar562@gmail.com>
2025-05-02 10:36:41 +0300

    add global ApiKeyGuard

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { ApiKeyGuard } from './common/guards/api-key.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -17,6 +18,7 @@ async function bootstrap() {
     }),
   );
   app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalGuards(new ApiKeyGuard());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
