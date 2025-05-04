7ad995820b2d61e25472b994c0e22b8e2cc6ead3
Andrey Kotlyar <kotlyar562@gmail.com>
2025-05-04 22:09:16 +0300

    e2e coffee test

--- test/coffee/coffees.e2e-spec.ts
+++ test/coffee/coffees.e2e-spec.ts
@@ -1,10 +1,20 @@
 import { CoffeesModule } from 'src/coffees/coffees.module';
+import { CreateCoffeeDto } from 'src/coffees/dto/create-coffee.dto';
+import { HttpExceptionFilter } from 'src/common/filters/http-exception.filter';
+import { TimeoutInterceptor } from 'src/common/interceptors/timeout.interceptor';
+import { WrapResponseInterceptor } from 'src/common/interceptors/wrap-response.interceptor';
+import * as request from 'supertest';
 
-import { INestApplication } from '@nestjs/common';
+import { HttpStatus, INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 describe('[Feature] Coffees - /coffees', () => {
+  const coffee = {
+    name: 'Test coffee',
+    brand: 'Test brand',
+    flavors: ['test flavor1', 'test flavor 2'],
+  };
   let app: INestApplication;
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -24,10 +34,44 @@ describe('[Feature] Coffees - /coffees', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+        transformOptions: {
+          enableImplicitConversion: true,
+        },
+      }),
+    );
+    app.useGlobalFilters(new HttpExceptionFilter());
+    app.useGlobalInterceptors(
+      new WrapResponseInterceptor(),
+      new TimeoutInterceptor(),
+    );
+
     await app.init();
   });
 
-  it.todo('Create [POST /]');
+  it('Create [POST /]', () => {
+    return request(app.getHttpServer())
+      .post('/coffees')
+      .set('Authorization', process.env.API_KEY || 'no_key')
+      .send(coffee as CreateCoffeeDto)
+      .expect(HttpStatus.CREATED);
+    // .then(({ body }) => {
+    //   console.log('BODY: ', body);
+    //   const expectedCoffee = expect.objectContaining({
+    //     ...coffee,
+    //     flavors: expect.arrayContaining(
+    //       coffee.flavors.map((name) => expect.objectContaining({ name })),
+    //     ),
+    //   });
+    //   console.log('BODY: ', body, expectedCoffee);
+    // expect(body).toEqual(expectedCoffee);
+    // });
+  });
   it.todo('Get all [GET /]');
   it.todo('Get one [GET /:id]');
   it.todo('Update one [PATCH /:id]');
